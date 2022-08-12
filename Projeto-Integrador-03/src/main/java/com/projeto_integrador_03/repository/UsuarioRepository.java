f428d807c9e4b5a986562b1165bedd61d01429a6
Vitor Martins <75342671+vitormartinsxd@users.noreply.github.com>
2022-08-12 09:37:22 -0300

    Delete Projeto-Integrador-03 directory
    Excluindo diretorio antigo.

--- Projeto-Integrador-03/src/main/java/com/projeto_integrador_03/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto_integrador_03.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto_integrador_03.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-}
